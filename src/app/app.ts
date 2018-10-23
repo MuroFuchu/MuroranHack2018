ab05c3065a5e532d83912802c31c66c1f794f872
AkifumiH <akifumi_hosaka@jsw-nis.co.jp>
2018-10-23 14:23:18 +0900

    Project's Initial Commit

---
+++ src/app/app.ts
@@ -0,0 +1,17 @@
+import {Component} from '@angular/core';
+import * as ons from 'onsenui';
+
+@Component({
+  selector: 'app',
+  template: require('./app.html'),
+  styles: [
+  	'./app.css'
+  ]
+})
+export class MyApp {
+  constructor() {}
+
+  alert() {
+    ons.notification.alert('This is an Onsen UI alert notification.');
+  }
+}

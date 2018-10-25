40c6cadd895009b18a16ceec2b9df61ad5052e27
羽山 大貴 <DaikiH@DEVELOPER.NISCO>
2018-10-25 14:25:06 +0900

    いろいろ修正

--- src/modules/children/timeTrip/timeTrip.ts
+++ src/modules/children/timeTrip/timeTrip.ts
@@ -1,21 +1,67 @@
-import {Component} from '@angular/core';
+import { OnInit,Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import * as ons from 'onsenui';
-
+import { Menu } from '../menu/menu';
+import {
+  OnsSelect,
+  OnsenModule,
+  CUSTOM_ELEMENTS_SCHEMA,
+  OnsNavigator,
+} from 'ngx-onsenui';
+ 
 @Component({
   selector: 'timeTrip',
   template: require('./TimeTrip.html'),
-  styles: [
-  	'./timeTrip.css'
-  ]
-})
+  styles: [`
+.right {
+  float: right;
+  margin-right: 3px;
+}
+ 
+.item {
+  margin-bottom: 10px;
+}
+
+.material-icons {
+  font-size:35px;
+}
+
+.btn {
+  color: black;
+  background-color: transparent;
+}
+  `]})
 export class TimeTrip {
-  constructor() {}
-
+  constructor(private _navigator: OnsNavigator) {}
+ 
+  adressList: Adress[] = [
+    { LocationID: "001", Address: "東京都府中市府中町１－１０", Latitude: 10, longitude: 10 },
+    { LocationID: "002", Address: "東京都府中市日鋼１－１", Latitude: 10, longitude: 10 },  
+    { LocationID: "003", Address: "東京都府中市東芝町５－１２－１", Latitude: 10, longitude: 10 },
+  ];
+ 
+  adress: Adress = null;
+ 
+  ngOnInit() {
+  　this.adress = this.adressList[0];
+  }
+  
   alert() {
-    ons.notification.alert('This is an Onsen UI alert notification.');
+  　ons.notification.alert('This is an Onsen UI alert notification.');
   }
-
+  
   alert2() {
-    ons.notification.toast('保坂参上！！');
+  　ons.notification.toast('保坂参上！！');
   }
+  
+  goToMenu() {
+  　this._navigator.nativeElement.pushPage(Menu, {data: {hoge: "fuga"}});
+  }
+}
+ 
+class Adress {
+  LocationID: string;
+  Address: string;
+  Latitude: number;
+  longitude: number;
 }

a865f16d13807f1a19c3b5f54e697e65fa09688e
羽山 大貴 <DaikiH@DEVELOPER.NISCO>
2018-10-26 20:37:20 +0900

    JSON取り込み

--- src/app/app.ts
+++ src/app/app.ts
@@ -23,37 +23,12 @@ export class MyApp {
 
     if(flg) {
       console.log('データ初期化するよ');
-      this.indexedDbService.addMultipleLocationInfo([
-        {
-            "LocationID": "1",
-            "Address": "北海道 室蘭市 常盤町１丁目",
-            "Latitude": 42.31841,
-            "Longitude": 140.9701178
-        },
-        {
-            "LocationID": "2",
-            "Address": "北海道 室蘭市 祝津町３丁目",
-            "Latitude": 42.339825,
-            "Longitude": 140.943745
-        },
-        {
-            "LocationID": "3",
-            "Address": "北海道 室蘭市 茶津町",
-            "Latitude": 42.319695,
-            "Longitude": 140.986877
-        },
-        {
-            "LocationID": "4",
-            "Address": "北海道 室蘭市 道道699号線",
-            "Latitude": 42.31891,
-            "Longitude": 140.97256
-        }
-      ]);
-  
-      //console.log(TrnPhotoInfo);
+      var MstLocationInfo = await this.getJsonService.GetJson('MstLocation').toPromise();
+      //console.log(MstLocationInfo);
+      this.indexedDbService.addMultipleLocationInfo(MstLocationInfo);
       
       var TrnPhotoInfo = await this.getJsonService.GetJson('TrnPhoto').toPromise();
-      console.log(TrnPhotoInfo);
+      //console.log(TrnPhotoInfo);
       this.indexedDbService.addMultiplePhotoInfo(TrnPhotoInfo);
       this.indexedDbService.cheakedInitData();
     }

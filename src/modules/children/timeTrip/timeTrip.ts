3229bf19a57b834fdac98a999ad3faa6a3225e72
羽山 大貴 <DaikiH@DEVELOPER.NISCO>
2018-10-26 19:42:13 +0900

    timetrip修正 getjson追加

--- src/modules/children/timeTrip/timeTrip.ts
+++ src/modules/children/timeTrip/timeTrip.ts
@@ -18,6 +18,7 @@ import { Upload } from '../upload/upload';
 @Component({
   selector: 'timeTrip',
   template: require('./timeTrip.html'),
+  // styleUrls: ['../src/modules/children/timeTrip/timeTrip.css'],
   styles: [`
   .left {
     float: left;
@@ -105,11 +106,13 @@ export class TimeTrip {
   @ViewChild('carousel') carousel;
   @ViewChild('modal') modal;
  
+  // テストデータ
   adressListDB: Adress[] = [
     { LocationID: "a001", Address: "北海道登別市幌別町３－２０", Latitude: 10, longitude: 10 },
     { LocationID: "a002", Address: "東京都府中市府中町１－９", Latitude: 20, longitude: 10 },  
     { LocationID: "a003", Address: "岩手県宮古市五月町１－１", Latitude: 50, longitude: 10 },
   ];
+  // テストデータ
   infoListDB: TimeTripPhotoInfo[] = [
     { PhotoId: "p001", Year: 1984, LocationID: "a001", Title: "幌別駅前通り 1984.", Comment: "北口駅前の様子。情調ある風景が感慨を感じさせる。", Bin: "aaa", LastUpdateDate: "20181025" },
     { PhotoId: "p002", Year: 2018, LocationID: "a001", Title: "幌別駅前通り 2018.", Comment: "北口駅前の様子。無機質な町並が現実を実感させる。", Bin: "d82m", LastUpdateDate: "20181025" },
@@ -118,7 +121,7 @@ export class TimeTrip {
     { PhotoId: "p005", Year: 2018, LocationID: "a002", Title: "京王府中一丁目ビル", Comment: "日鋼情報システム(株)が本社JSWに吸収合併されることが決定した年。", Bin: "d82m", LastUpdateDate: "20181025" },
     { PhotoId: "p006", Year: 2062, LocationID: "a002", Title: "京王府中一丁目ビル", Comment: "本社JSWが事実上日本を支配した年。", Bin: "k8aa", LastUpdateDate: "20181025" },
     { PhotoId: "p007", Year: 1978, LocationID: "a003", Title: "宮古市魚菜市場　開業", Comment: "宮古市魚菜市場が開業しました。「宮古市をみんなで盛り上げよう」というモットーのもと地元自治体と地元漁師農家の協力によって営業運営される市場です。", Bin: "sj01", LastUpdateDate: "20181025" },
-    { PhotoId: "p008", Year: 2018, LocationID: "a003", Title: "宮古市魚菜市場　開業30周年", Comment: "宮古市魚菜市場が開業30周年を迎えました。日本有数の漁場である三陸沖から水揚げされる魚介類と、地元農家が愛情をこめて育てた野菜がずらりと並びます。", Bin: "d82m", LastUpdateDate: "20181025" },
+    { PhotoId: "p008", Year: 2019, LocationID: "a003", Title: "宮古市魚菜市場　開業30周年", Comment: "宮古市魚菜市場が開業30周年を迎えました。日本有数の漁場である三陸沖から水揚げされる魚介類と、地元農家が愛情をこめて育てた野菜がずらりと並びます。", Bin: "d82m", LastUpdateDate: "20181025" },
     { PhotoId: "p009", Year: 2088, LocationID: "a003", Title: "宮古市魚菜市場　開業100周年", Comment: "宮古市魚菜市場が開業100周年を迎えました。人類が食事をやめ必要なエネルギーのみをインポートする時代でも、昔と変わらず新鮮な食材を売り続けています。", Bin: "k8aa", LastUpdateDate: "20181025" },
   ];
  
@@ -128,26 +131,29 @@ export class TimeTrip {
   info: TimeTripPhotoInfo = null;
   targetYear: number = 0;
 
+  activeIndex: number = 0;
   isImgErrList: boolean[] = [];
 
   constructor(private _navigator: OnsNavigator, private _indexedDbService: IndexedDbService, private _params: Params) {}
  
   async ngOnInit() {
-    console.log(this._params);
     // 遷移元画面から位置情報マスタを取得
-  // 　this.adress = this.adressListDB[0];
-    // this.locationId = this.adress.LocationID;
     this.targetYear = this._params.data.year;
     this.locationId = this._params.data.LocationID;
     this.adress = this.adressListDB.find(f => f.LocationID == this.locationId);
 
+    // timeTrip情報を設定
     this.setInfoList(this.adress);
 
-    var index = this.infoList.findIndex(s => s.PhotoId == this.info.PhotoId);
-    this.carousel.nativeElement.setAttribute("initial-index", index.toString());
+    // carouselの初期位置設定
+    this.activeIndex = this.infoList.findIndex(s => s.PhotoId == this.info.PhotoId);
+    this.carousel.nativeElement.setAttribute("initial-index", this.activeIndex.toString());
 
+    // テストで画像を取得してみる
     var res = await this._indexedDbService.getTrnPhotoInfoByKey("1");
     this.infoListDB[0].Bin = res.Bin;
+    this.infoListDB[1].Bin = res.Bin;
+    this.infoListDB[2].Bin = res.Bin;
   }
 
   toPostChane(event) {
@@ -178,11 +184,11 @@ export class TimeTrip {
   }
   
   goToMenu() {
-  　this._navigator.nativeElement.pushPage(Menu, {data: {hoge: "fuga"}});
+  　this._navigator.nativeElement.pushPage(Menu);
   }
 
   goToUpload() {
-    this._navigator.nativeElement.pushPage(Upload, {data: {hoge: "fuga"}});
+    this._navigator.nativeElement.pushPage(Upload, {data: { Year: this.targetYear, LocationID: this.locationId}});
   }
 
   convertImageSource(bin: string) {

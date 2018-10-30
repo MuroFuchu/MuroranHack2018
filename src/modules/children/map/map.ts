00e3d83347a35350f7bca5009221beaea1144baa
Takuma Ohira <takumao@jsw-nis.co.jp>
2018-10-30 15:22:15 +0900

    a

--- src/modules/children/map/map.ts
+++ src/modules/children/map/map.ts
@@ -133,6 +133,7 @@ export class Map {
       this.markers = [];
     }else{
       data.forEach(data => {
+        console.log(data.Title);
         this.markers.push(
           { LocationID:data.LocationID,
             Title:data.Title,
